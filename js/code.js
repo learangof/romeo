41b9dda026723276cb8a7b0a1272f9abf87fdf0d
Luis Arango <learangof@outlook.com>
2020-08-23 19:54:27 +1200

    finish

--- js/code.js
+++ js/code.js
@@ -16,6 +16,14 @@ function checkPhoneNumber(txt) {
   var phone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
   return checkWithRegularExpression(txt, phone);
 }
+function checkCCV(txt) {
+  var ccv = /^[0-9]{3}$/;
+  return checkWithRegularExpression(txt, ccv);
+}
+function checkCC(txt) {
+  var ccv = /^\(?([0-9]{4})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})?[-. ]?([0-9]{4})$/;
+  return checkWithRegularExpression(txt, ccv);
+}
 function checkEmail(txt) {
   var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return checkWithRegularExpression(txt, email);
@@ -39,6 +47,7 @@ function isvalid(txt, valid) {
     return false;
   }
 }
+
 function validateForm(form) {
   var val = true;
   for (var i = 0; i < form.length; i++) {
@@ -52,16 +61,15 @@ function validateForm(form) {
         break;
     }
   }
-  
-  if(val){
-    setTimeout(sendForm,500,form);
-    
+
+  if (val) {
+    setTimeout(sendForm, 500, form,'#succesModal');
   }
 }
-function sendForm(form){
+function sendForm(form,modalName) {
   //alert("We will answer you as soon as posible\n(wink, wink)\n");
   //form.submit();
-  $('#succesModal').modal();
+  $(modalName).modal();
   resetForm(form);
 }
 function resetForm(form) {
@@ -78,14 +86,73 @@ function resetForm(form) {
       form[i].classList.remove("is-invalid");
       form[i].classList.remove("is-valid");
     }
+    if ((form[i].type === "checkbox")) {
+      form[i].checked = false;
+      form[i].classList.remove("is-invalid");
+      form[i].classList.remove("is-valid");
+    }
+    if ((form[i].type === "radio")) {
+      form[i].checked = false;
+      form[i].classList.remove("is-invalid");
+      form[i].classList.remove("is-valid");
+    }
+    if((form[i].type === "select-one")){
+      form[i].selectedIndex = 0;
+      form[i].classList.remove("is-invalid");
+      form[i].classList.remove("is-valid");
+    }
   }
- }
+}
 
- function lol(form){
-  for (var i = 0; i < form.length; i++) {
-    alert(form[i].type);
+function validatePayment(form) {
+  var val = true;
+  var rsltRbtn = validateRadiobtn(form.ccRadioOptions);
+  val &= rsltRbtn[0];
+  val &= validateCheckbtn(form.agreeChckBtn)
+  val &= validateDate([form.cYear, form.cMonth]);
+  val &= checkCCV(form.ccv);
+  val &= checkCC(form.cc);
+  val &= checkEmail(form.user_email);
+  var userInfo = form.user_info;
+  for(i = 0; i < userInfo.length; i++){
+    val &= checkEmpty(userInfo[i]);
   }
- }
+  if(val){
+    document.getElementById("uName").innerHTML = userInfo[0].value;
+    document.getElementById("uCCT").innerHTML = rsltRbtn[1];
+    document.getElementById("CCN").innerHTML = form.cc.value.slice(12, 16);
+    document.getElementById("uEmail").innerHTML = form.user_email.value;
+    document.getElementById("uAdd").innerHTML = userInfo[1].value;
+    setTimeout(sendForm, 500, form,'#succesPayment');
+  }
+}
+
+function validateDate(cardDate) {
+  var date = new Date(parseInt(cardDate[0].value), (parseInt(cardDate[1].value) - 1));
+  if (new Date() > date) {
+    isvalid(cardDate[0], false);
+    return isvalid(cardDate[1], false);
+  } else {
+    isvalid(cardDate[0], true);
+    return isvalid(cardDate[1], true);
+  }
+}
+function validateCheckbtn(checkbtns) {
+
+  if (checkbtns.checked == true) {
+    return isvalid(checkbtns, true);
+  } else {
+    return isvalid(checkbtns, false);
+  }
+}
+function validateRadiobtn(radiobtns) {
+  for (var i = 0; i < radiobtns.length; i++) {
+    if ((radiobtns[i].checked == true)) {
+      return [isvalid(radiobtns[2], true), radiobtns[i].value];
+    }
+  }
+  return [isvalid(radiobtns[2], false), null];
+}
 function clearForm(txtArray) {
   txtArray.forEach((e) => {
     e.value = "";
