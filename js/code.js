71795cadf5501ec2f5422e6e4ea7f19829221522
Luis Arango <learangof@outlook.com>
2020-08-24 19:46:52 +1200

    finish

--- js/code.js
+++ js/code.js
@@ -13,7 +13,7 @@ function checkWithRegularExpression(txt, expression) {
 }
 
 function checkPhoneNumber(txt) {
-  var phone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  var phone = /^\(?([0]{1})\)?[-. ]?([2]{1})?[-. ]?([0-9]{1})?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
   return checkWithRegularExpression(txt, phone);
 }
 function checkCCV(txt) {
